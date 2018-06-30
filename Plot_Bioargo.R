6c6560798493036e49313d569fb314c7e7df29a9
mishahu-ecosmith <mishahu-ecosmith@localhost.localdomain>
2018-06-30 14:25:03 +0530

    This is an overview plot. still need to decorate with more parameters and legends

---
+++ Plot_Bioargo.R
@@ -0,0 +1,46 @@
+#===============================================================
+#Plotting functions for BioArgo
+#
+#===================================================================
+# plotting overview of graphs, with options to plot single graphs
+#This function returns all the plots in a single functions
+# Always be trimmed before plotting according to your needs
+
+plot_BioArgo<- function(x,temperature=FALSE,salinity=FALSE,
+                        oxygen=FALSE,chlorophyll=FALSE)
+  {
+# make it true, when you want only temperature
+   if(temperature==TRUE){
+    plot(x[[5]],-x[[4]],xlab = names(x)[5], ylab = names(x)[4])}
+  # make it true, when you want only salinity
+ else
+  if(salinity==TRUE){
+    plot(x[[6]],-x[[4]],xlab = names(x)[6], ylab = names(x)[4])}
+  # make it true, when you want only oxygen
+  else
+  if(oxygen==TRUE){
+    plot(x[[7]],-x[[4]],xlab = names(x)[7], ylab = names(x)[4])}
+  # make it true, when you want only chlorophyll
+  else
+  if(chlorophyll==TRUE){
+    plot(x[[8]],-x[[4]],xlab = names(x)[8], ylab = names(x)[4])}
+ #if you don't have any option this wil plot the whole graph in a panel
+   else
+  {
+
+    par(mfrow=c(2,2))
+    plot(x[[5]],-x[[4]],xlab = names(x)[5], ylab = names(x)[4],xaxt="n",type = "l")
+    axis(3)
+    axis(2)
+    plot(x[[6]],-x[[4]],xlab = names(x)[6], ylab = names(x)[4],xaxt="n",yaxt="n",type = "l")
+    axis(3)
+    axis(2)
+    plot(x[[7]],-x[[4]],xlab = names(x)[7], ylab = names(x)[4],xaxt="n",yaxt="n",type = "l")
+    axis(3)
+    axis(2)
+    plot(x[[8]],-x[[4]],xlab = names(x)[8], ylab = names(x)[4],xaxt="n",yaxt="n",type = "l")
+    axis(3)
+    axis(2)
+  }
+  }
+
