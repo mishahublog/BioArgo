4d0f38f1c504f8f820865ff990f61308125956aa
mishahu-ecosmith <mishahu-ecosmith@localhost.localdomain>
2018-07-04 12:46:35 +0530

    For batch listing  from filenemaes, Applications for section plots,o r with other multiple plots

---
+++ batchlisting.R
@@ -0,0 +1,11 @@
+####++++++++++++++++++++++===========================================================
+####
+### Batch processsing of Argo files 
+##====================================================================================
+
+batch<- function(filenames){
+
+batchlist<- lapply(filenames,function(x)ExtractBioArgo(x))
+names(batchlist)<- filenames
+return(batchlist)
+}
