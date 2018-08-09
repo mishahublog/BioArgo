402e7ccc7a92a01fc2a2f7d954d4413600d6ce53
mishahublog <mishahublog@gmail.com>
2018-08-09 10:31:00 +0530

    Document updated

--- ponman/R/Filter_bioArgo.R
+++ ponman/R/Filter_bioArgo.R
@@ -9,9 +9,11 @@
 #'
 #'@param batchlist A list of Argo profiles return from  \code{\link{batch()}}
 #'@param parameter A defined number of variables, the formats are shown below
-#'@param start     A value,in which your range starts
-#'@param end       A value,in which your range ends
-#'
+#'@param start     A value,in which your range starts, Not valued when parameter is "date"
+#'@param end       A value,in which your range ends,Not valued when parameter is "date"
+#'@param month     A value as "character", when parameter is "date"
+#'@param day       A value as "character", when parameter is "date"
+#'@param year      A value as "character", when parameter is "date"
 #' 
 #'@format 
 #'
@@ -24,8 +26,22 @@
 #'   \item Chlorophyll - "chlorophyll"
 #'   \item Backscatter - "backscatter"
 #' }
+#' Parameter "date" formating for months
+#' \itemize{
+#'   \item January  -"01"
+#'   \item February -"02"
+#'   \item March    -"03"
+#'   \item . . . ..
+#'   \item December -"12"
+#' }
+#' 
 #'@author Midhun shah Hussain
 #' 
+#'@examples Argo_February<- Filter_bioArgo(test,parameter = "date",month = "02")#select months
+#'@examples Argo_2013<- Filter_bioArgo(test,parameter = "date",year = "2013")#select years
+#'@examples depth20.30<- Filter_bioArgo(test,parameter = "pressure",start = 20,end = 30)# Be careful that the values are numeric
+#'
+#'
 #' 
 #' 
 Filter_bioArgo<- function(batchlist,parameter,start=NULL,end=NULL,month=NULL, year=NULL,day=NULL){
---
+++ ponman/R/batchlisting.R
@@ -0,0 +1,30 @@
+####++++++++++++++++++++++===========================================================
+####
+### Batch processsing of Argo files 
+##====================================================================================
+#'
+#'Batch listing of Argo profiles
+#'
+#'
+#'Listing of Argo profiles in a batch, it helps in collective analysis of specific sections. Very useful in making time series, contour and filled section contours
+#'
+#'@param filenames list of Argo profiles(.nc) from the working directory
+#'
+#'
+#'@author Midhun shah Hussain
+#'
+#'@examples
+#'
+#'profiles<- list.files()# set working directory where Argo profiles stored
+#'
+#'profilelist<- batch(profiles)# A list of Argo profiles
+#'
+#'
+batch<- function(){
+  
+batchlist<- lapply(filenames,function(x)ExtractBioArgo(x))
+names(batchlist)<- filenames
+return(batchlist)}
+
+
+
