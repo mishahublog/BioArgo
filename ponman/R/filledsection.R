8ac946a0c06d80cf375f6db7905096b5fae490ff
mishahublog <mishahublog@gmail.com>
2018-09-01 23:47:15 +0530

    mld added

--- ponman/R/filledsection.R
+++ ponman/R/filledsection.R
@@ -37,16 +37,7 @@
 #' legend("bottom",legend = "temperature",lwd = 1)#Adding Legend
 #'
 #'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-Filledsectionplots<- function(Argolist,parameter,col.val,overlay=TRUE,...)
+Filledsectionplots<- function(Argolist,parameter,col.val,overlay=TRUE,mld=FALSE,...)
   #Argolist is a list BioArgo extracted as a output from extractbioArgo function
   #Parameter defines the parameters such as temp, sal,etc
   #col.val amount of colours used in the palette, Here we used matlab.like from colourramp
@@ -55,7 +46,9 @@ Filledsectionplots<- function(Argolist,parameter,col.val,overlay=TRUE,...)
 Most_freq <- function(x) {
     ux <- unique(x)
     ux[which.max(tabulate(match(x, ux)))]
-   }
+}
+#data formating
+#==============
 listlat<- lapply(1:length(Argolist),function(x)Most_freq(c(Argolist[[x]]$latitude,rep(NA,Mod(length(Argolist[[x]]$latitude)-(maxfac))))))
 listlon<- lapply(1:length(Argolist),function(x)Most_freq(c(Argolist[[x]]$longitude,rep(NA,Mod(length(Argolist[[x]]$longitude)-(maxfac))))))
 listtemp<- lapply(1:length(Argolist),function(x)c(Argolist[[x]]$temperature,rep(NA,Mod(length(Argolist[[x]]$temperature)-(maxfac)))))
@@ -81,13 +74,24 @@ xlist<- list(cycleno,nam,date.section,lat.section,lon.section,depth.section)
 #xdata<- data.frame(x$date,x$lon,x$lat,x$depth)
 xdata1<- data.frame(lapply(xlist,function(x)ldply(x)))
 names(xdata1)<- c("cycle.no","id","date","lon","lat","depth")
+##derived coeffieicnts======================
+find_mld<- function(Argoprofile){ 
+  library(gsw)
+  sigma<- gsw_sigma0(SA = Argoprofile$salinity,CT = Argoprofile$temperature)
+  dat<- data.frame(sigma,pressure=Argoprofile$pressure)
+  pres<- rownames(subset(dat,dat$pressure>=9 & Argoprofile$pressure<=10))[1]
+  sig03<- sigma[as.numeric(pres)]+0.03
+  mld<- subset(dat,dat$sigma>=sig03 )[2,2]
+  return(mld)
+}
 
-
+mldlist<- lapply(1:length(test),function(x)find_mld(test[[x]]))
+datmld<- data.frame(x=1:length(Argolist),mld=as.numeric(mldlist))
 
 #=================================================
 source("Contour.overlay.R")
 #===============================================
-
+if(mld==FALSE){
 
 if(parameter=="temperature" ){
   mattemp<- as.matrix(as.data.frame(listtemp))
@@ -111,7 +115,6 @@ if(parameter=="temperature" ){
                    axis(2,at=seq(1,totaldepthno,length.out = 25),
                         labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))));
                    Contour.overlay(Argolist,parameter = overlay,add=TRUE)
-                   
                      },
                  key.title = title(main=expression(~degree~C),cex.main=1),
                  key.axes = axis(4, seq(mintemp,maxtemp, length.out =  10)),...)
@@ -141,7 +144,7 @@ else
                    axis(side = 1,at= 1:length(Argolist),labels = seq(date[[1]],date[[length(Argolist)]],length.out = length(Argolist)))
                    axis(2,at=seq(1,totaldepthno,length.out = 25),
                    labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))))
-                   ;Contour.overlay(test,parameter = overlay,add=TRUE)
+                   ;Contour.overlay(Argolist,parameter = overlay,add=TRUE)
                    },
                    key.title = title(main="PSU"),
                    key.axes = axis(4, seq(minsal, maxsal, length.out =  10))
@@ -204,11 +207,160 @@ if(parameter=="chlorophyll" ){
                  key.title = title(main=expression("mg"/{m}^3)),
                  key.axes = axis(4, seq(minchl,maxchl, length.out =  10)),...)
   
-  return(xdata1)}
-#+++++++++++++++++++++=============================================================================================
+  return(xdata1)}}
+
+##MLD===================================
+if (mld ==TRUE){
+  
+  if(length(depth[[1]])>29){stop("trim up to 100")}
+   
+  
+  {if(parameter=="temperature" ){
+  mattemp<- as.matrix(as.data.frame(listtemp))
+  colnames(mattemp)<- NULL
+  totaldepthno<- max(as.numeric(lapply(1:length(Argolist),function(x)length(Argolist[[x]]$pressure))))
+  depth<- lapply(1:length(Argolist),function(x)c(Argolist[[x]]$pressure,rep(NA,Mod(length(Argolist[[x]]$pressure)-(maxfac)))))
+  maxdepth<- max(as.numeric(lapply(depth, function(x) x[which.max(abs(x))])))
+  mindepth<- min(as.numeric(lapply(depth, function(x) x[which.min(abs(x))])))
+  mintemp<- min(as.numeric(lapply(listtemp, function(x) x[which.min(abs(x))])))
+  maxtemp<- max(as.numeric(lapply(listtemp, function(x) x[which.max(abs(x))])))
+  library(colorRamps)
+  
+  par(mar=c(4,5,2,2))
+  f <- function(m) t(m)[,nrow(m):1]
+  filled.contour(x = 1:nrow(f(mattemp)),y = 1:ncol(f(mattemp)),
+                 z =f(mattemp), col = col.val,
+                 plot.title = title(main = "",
+                                    xlab = "Date",ylab = "Depth(m)"),
+                 plot.axes = {
+                   axis(side = 1,at= 1:length(Argolist),labels = seq(date[[1]],date[[length(Argolist)]],length.out = length(Argolist)))
+                   axis(2,at=seq(1,totaldepthno,length.out = 25),
+                        labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))));
+                   par(new=TRUE);
+                   plot(datmld$x,-datmld$mld,xaxt="n",yaxt="n",xlab="",ylab="",type="l")
+                 },
+                 key.title = title(main=expression(~degree~C),cex.main=1),
+                 key.axes = axis(4, seq(mintemp,maxtemp, length.out =  10)),...)
+  
+  return(xdata1)
+
+} 
+  else
+    
+    if(parameter=="salinity" ){
+      matsal<- as.matrix(as.data.frame(listsal))
+      colnames(matsal)<- NULL
+      totaldepthno<- max(as.numeric(lapply(1:length(Argolist),function(x)length(Argolist[[x]]$pressure))))
+      depth<- lapply(1:length(Argolist),function(x)c(Argolist[[x]]$pressure,rep(NA,Mod(length(Argolist[[x]]$pressure)-(maxfac)))))
+      maxdepth<- max(as.numeric(lapply(depth, function(x) x[which.max(abs(x))])))
+      mindepth<- max(as.numeric(lapply(depth, function(x) x[which.min(abs(x))])))
+      minsal<- min(as.numeric(lapply(listsal, function(x) x[which.min(abs(x))])))
+      maxsal<- max(as.numeric(lapply(listsal, function(x) x[which.max(abs(x))])))
+      
+      par(mar=c(4,5,2,2))
+      f <- function(m) t(m)[,nrow(m):1]
+      filled.contour(x = 1:nrow(f(matsal)),y = 1:ncol(f(matsal)),
+                     z =f(matsal), col =  col.val,
+                     plot.title = title(main = "",
+                                        xlab = "Date",ylab = "Depth(m)"),
+                     plot.axes = {
+                       axis(side = 1,at= 1:length(Argolist),labels = seq(date[[1]],date[[length(Argolist)]],length.out = length(Argolist)))
+                       axis(2,at=seq(1,totaldepthno,length.out = 25),
+                            labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))))
+                       ; par(new=TRUE);
+                       plot(datmld$x,-datmld$mld,xaxt="n",yaxt="n",xlab="",ylab="",type="l")
+                     },
+                     key.title = title(main="PSU"),
+                     key.axes = axis(4, seq(minsal, maxsal, length.out =  10))
+                     ,...)
+      
+      return(xdata1)
+      
+    } else
+      
+      if(parameter=="oxygen" ){
+        matoxy<- as.matrix(as.data.frame(listoxy))
+        colnames(matoxy)<- NULL
+        totaldepthno<- max(as.numeric(lapply(1:length(Argolist),function(x)length(Argolist[[x]]$pressure))))
+        depth<- lapply(1:length(Argolist),function(x)c(Argolist[[x]]$pressure,rep(NA,Mod(length(Argolist[[x]]$pressure)-(maxfac)))))
+        maxdepth<- max(as.numeric(lapply(depth, function(x) x[which.max(abs(x))])))
+        mindepth<- max(as.numeric(lapply(depth, function(x) x[which.min(abs(x))])))
+        minoxy<- min(as.numeric(lapply(listoxy, function(x) x[which.min(abs(x))])))
+        maxoxy<- max(as.numeric(lapply(listoxy, function(x) x[which.max(abs(x))])))
+        
+        par(mar=c(4,5,2,2))
+        f <- function(m) t(m)[,nrow(m):1]
+        filled.contour(x = 1:nrow(f(matoxy)),y = 1:ncol(f(matoxy)),
+                       z =f(matoxy), col =  col.val,
+                       plot.title = title(main = "",
+                                          xlab = "Date",ylab = "Depth(m)"),
+                       plot.axes = {
+                         axis(side = 1,at= 1:length(Argolist),labels = seq(date[[1]],date[[length(Argolist)]],length.out = length(Argolist)))
+                         axis(2,at=seq(1,totaldepthno,length.out = 25),
+                              labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))));
+                         par(new=TRUE);
+                         plot(datmld$x,-datmld$mld,xaxt="n",yaxt="n",xlab="",ylab="",type="l")
+                       },
+                       key.title = title(main=expression(paste(mu,"mol/kg")),cex.main=1),
+                       key.axes = axis(4, seq(minoxy, maxoxy, length.out =  10)),...)
+        
+        return(xdata1)
+      }
+  if(parameter=="chlorophyll" ){
+    matchl<- as.matrix(as.data.frame(listchl))
+    colnames(matchl)<- NULL
+    totaldepthno<- max(as.numeric(lapply(1:length(Argolist),function(x)length(Argolist[[x]]$pressure))))
+    depth<- lapply(1:length(Argolist),function(x)c(Argolist[[x]]$pressure,rep(NA,Mod(length(Argolist[[x]]$pressure)-(maxfac)))))
+    maxdepth<- max(as.numeric(lapply(depth, function(x) x[which.max(abs(x))])))
+    mindepth<- max(as.numeric(lapply(depth, function(x) x[which.min(abs(x))])))
+    minchl<- min(as.numeric(lapply(listchl, function(x) x[which.min(abs(x))])))
+    maxchl<- max(as.numeric(lapply(listchl, function(x) x[which.max(abs(x))])))
+    
+    par(mar=c(4,5,2,2))
+    f <- function(m) t(m)[,nrow(m):1]
+    filled.contour(x = 1:nrow(f(matchl)),y = 1:ncol(f(matchl)),
+                   z =f(matchl), col=  col.val,
+                   plot.title = title(main = "",
+                                      xlab = "Date",ylab = "Depth(m)"),
+                   plot.axes = {
+                     axis(side = 1,at= 1:length(Argolist),labels = seq(date[[1]],date[[length(Argolist)]],length.out = length(Argolist)))
+                     axis(2,at=seq(1,totaldepthno,length.out = 25),
+                          labels = rev(as.integer(seq(mindepth,maxdepth,length.out = 25))));
+                    par(new=TRUE);
+                    plot(datmld$x,-datmld$mld,xaxt="n",yaxt="n",xlab="",ylab="",type="l")
+                   }
+                   ,
+                   key.title = title(main=expression("mg"/{m}^3)),
+                   key.axes = axis(4, seq(minchl,maxchl, length.out =  10)),...)
+    
+    return(xdata1)}}
+
+
+}}
+#+++++++++++++++++++++============================================================================================
 # without limits
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
