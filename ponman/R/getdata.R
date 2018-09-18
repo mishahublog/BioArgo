a0f3e5cd51d6618d8018a19248ea4b1cc11514f6
mishahublog <mishahublog@gmail.com>
2018-09-18 13:18:50 +0530

    getdata

--- ponman/R/getdata.R
+++ ponman/R/getdata.R
@@ -1,5 +1,33 @@
 
-
+#'Get data from FTP servers of ifremier to your PC
+#'
+#'
+#'
+#'
+#'@description 
+#'
+#'This function enables to access datasets from FTP servers from ifremer.It helps to access databases on two different modes.
+#'
+#'@param mode This argument switchs on two different ways to access the datasets
+#'
+#' \itemize{
+#'   \item base    - "Access data using database and platform informations"
+#'   \item geotime - "Access data by location, year and month"} 
+#'
+#'@param location 
+#'@param year
+#'@param month
+#'@param database
+#'@param platform
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
 
 get_data2ponman<- function(mode,location=NULL,year=NULL,month=NULL,database=NULL,platform=NULL){
 library(curl)
@@ -18,7 +46,6 @@ library(curl)
   if(quest== 2) {
     quest2<- readline(prompt = "Type the file name you want to download:  ")
        download.file(paste(url,quest2,sep=""),destfile = quest2)
-  
   }
 }
 
